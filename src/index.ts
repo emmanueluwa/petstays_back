a6d4875156ed45bf91493f7020188b5afab6336c
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-22 08:02:48 +0100

    adding functionality for creation of a user account

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to db :)"));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/api/users", userRoutes);
+
+app.listen(7000, () => {
+  console.log("server running on localhost:7000 :)");
+});
